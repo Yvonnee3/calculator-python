7afef7a8dcee4f9f423f8f496e1e3c5e7913f815
Yvonnee3 <yvonnemueni42@gamil.com>
2025-03-23 19:57:52 +0300

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num1=9
+num2=3
+# 1 - Addition
+print(num1 + num2)
+
+# 2 - Subtraction
+print(num1 - num2)
+
+# 3 - Multiplication
+print(num1 * num2)
+
+# 4 - Division
+print(num1 / num2)
+
+    
